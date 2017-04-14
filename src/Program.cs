5f31c294a7f7e76819e547c477f16df383240c54
Marvin Andia <marvin.andia@cignium.com>
2017-04-13 19:14:57 -0500

    optimize docker file for prod

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,8 @@
+using System;
 using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace restaurant_dashboard_backend
 {
@@ -8,15 +10,31 @@ namespace restaurant_dashboard_backend
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()  
+            .SetBasePath(GetContentRoot())
+            .AddJsonFile("hosting.json", optional: true)
+            .Build();
+
             var host = new WebHostBuilder()
+                .UseConfiguration(config)
                 .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseUrls("http://*:5000")
+                .UseContentRoot(GetContentRoot())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
 
             host.Run();
+        }
+
+        private static string GetContentRoot() {
+            var root = Directory.GetCurrentDirectory();
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            if(env == "production") {
+                root += "/dist";
+            }
+
+            return root;
         }
     }
 }
